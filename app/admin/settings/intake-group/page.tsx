162ede9d75d9ecc01a155a7a621701512436c431
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2025-01-27 15:25:26 +0200

    feat: add IntakeGroupManager component to intake group settings page

--- app/admin/settings/intake-group/page.tsx
+++ app/admin/settings/intake-group/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import AddIntakeGroup from "@/components/forms/intakeGroups/IntakeGroupManager";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <AddIntakeGroup />
+    </div>
+  );
+};
 
-export default page
+export default page;
