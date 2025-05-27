43bb8bee6fdc5c487ef7fe53f3ecf7f877ad594a
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2025-05-27 10:42:25 +0200

    Refactor code structure for improved readability and maintainability

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -50,12 +50,12 @@ export default function LoginPage() {
       {/* Right section: Background image, now proportional */}
       <div className="hidden md:block w-1/2 lg:w-2/3 rounded-bl-[250px] md:rounded-bl-[150px] lg:rounded-bl-[200px] overflow-hidden relative h-screen">
         <Image
-          src="/img/auth/Front-img-7.jpeg"
+          src="/img/auth/Front-img-8.jpeg"
           alt="Auth background"
           fill
           sizes="(max-width: 768px) 0vw, (max-width: 1024px) 50vw, 66vw"
           priority
-          className="object-cover"
+          className="object-cover object-left"
         />
       </div>
     </div>
